cce7019e614c5406826406c002e53c5b0fbfc7be
skzaslow <skzaslow@gmail.com>
2012-11-10 12:58:42 -0600

    user model and attribute validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :first, :last
+  
+  validates :first, presence: true, length: { maximum: 50}
+  validates :last, presence: true, length: { maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  
   has_many :entries
 end
