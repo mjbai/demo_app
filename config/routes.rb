4262609a4934452f3c6a0ba52b3e4b968ecca885
Julie Bai <bai.julie@gmail.com>
2012-11-10 20:54:35 -0500

    Finish sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,11 @@ DemoApp::Application.routes.draw do
   match '/help', to: 'static_pages#help'
   match '/about', to: 'static_pages#about'
   match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   resources :entries
+  resources :sessions, only: [:new, :create, :destroy]
 
   resources :users
 
