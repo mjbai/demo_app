7f9e2284ecd987dfebe5a5664795f5ac9a7f20ed
Julie Bai <bai.julie@gmail.com>
2012-11-15 15:23:10 -0500

    Added Shira's CSS changes

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,9 +1,20 @@
+#require 'rest-open-uri'
+
 class Entry < ActiveRecord::Base
+
   attr_accessible :context, :definition, :example_sentence, :image,  :word
-  belongs_to :user
+  belongs_to :users
+  #has_attached_file :image
   
   validates :word, presence: true, length: { maximum: 50} 
   
   validates :user_id, presence: true
   default_scope order: 'entries.created_at DESC'
+
+  private
+  #Get the picture from a given url. 
+   def picture_from_url(url)
+      self.image = open(url)
+   end
+
 end
